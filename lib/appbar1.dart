d94d0b3b2d009820c0ae0a4e73d830ee36bc35d7
thuvan <thuvan4542@gmail.com>
2024-03-12 21:33:09 +0700

    First commit

---
+++ lib/appbar1.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Title of Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(
+        title: '',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //int _counter = 0;
+
+  //void _incrementCounter() {
+  //setState(() {
+  // This call to setState tells the Flutter framework that something has
+  // changed in this State, which causes it to rerun the build method below
+  // so that the display can reflect the updated values. If we changed
+  // _counter without calling setState(), then the build method would not be
+  // called again, and so nothing would appear to happen.
+  //_counter++;
+  //});
+  //}
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(title: const IconTitleWidget()),
+      body: const Center(
+          child: Text(
+        'Flutter AppBar Tutorial',
+      )),
+    );
+  }
+}
+
+class IconTitleWidget extends StatelessWidget {
+  const IconTitleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    imageCache.clear();
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.center, // Centers horizontally
+      crossAxisAlignment: CrossAxisAlignment.center, // Centers vertically
+      children: <Widget>[
+        Icon(Icons.train),
+        Icon(Icons.place),
+        // The SizedBox provides an immediate spacing between the widgets
+        SizedBox(
+          width: 3,
+        ),
+        Text(
+          "Place",
+        )
+      ],
+    );
+  }
+}
